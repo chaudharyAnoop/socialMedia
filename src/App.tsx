d9dfe56cc3c6a9364c1f1453d5116cbae1c1275f
Lakshay Agarwal <lakshay.agarwal1@appinventiv.com>
2025-06-12 10:29:20 +0530

    managed unused hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 import styles from "./App.module.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
-// import LandingPage from "./pages/LandingPage/LandingPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import QuickAccess from "./components/QuickAccess/QuickAccess";
-import Signin from "./pages/Signin/Signin";
+
 function App() {
   // const [count, setCount] = useState(0);
 
@@ -14,8 +14,8 @@ function App() {
               <div className={styles.nav}>
                   <NavigationBar />
                   <Routes>
-                      {/* <Route path="/" element={<LandingPage />} /> */}
-                      <Route path="/" element={<Signin/>} />
+                      <Route path="/" element={<LandingPage />} />
+                      
                   </Routes>
                   <QuickAccess />
               </div>
