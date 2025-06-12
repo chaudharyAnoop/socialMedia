dc7a97b22cf17c4bca0fa5f3d020f741730abef7
Phinxaura <yash.rajput1@appinventiv.com>
2025-06-12 11:36:33 +0530

    conflictresolve

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import QuickAccess from "./components/QuickAccess/QuickAccess";
 // import AdminPage from "./pages/AdminPage/AdminPage";
 import { Suspense, lazy } from "react";
 import RouteChangeHandler from "./components/RouteChangeHandler";
+import ExplorePage from "./pages/explore/ExplorePage";
 
 const LandingPage = lazy(() => import("./pages/LandingPage/LandingPage"));
 const AdminPage = lazy(() => import("./pages/AdminPage/AdminPage"));
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useRef, useCallback } from "react";
 import styles from "./LandingPage.module.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -82,16 +81,6 @@ function LandingPage() {
       )}
     </div>
   );
-=======
-import LandingPageOne from "../../components/LandingPage.component";
-
-function LandingPage() {
-    return (
-        <div>
-            <LandingPageOne />
-        </div>
-    );
->>>>>>> feature/signin-signup
 }
 
 export default LandingPage;
